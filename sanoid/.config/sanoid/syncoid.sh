b5e1f25476ebfb1fb338ab09bef35adbfc2330fe
styxut <styxut@gmail.com>
2025-02-04 16:22:39 -0700

    update sanoid, syncoid, and snapshot pruning configs

--- sanoid/.config/sanoid/syncoid.sh
+++ sanoid/.config/sanoid/syncoid.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 sudo syncoid --compress=none zroot/ROOT/arch_hyprland zfs-pool-WD1TB-1/zfs-backups/arch_hyprland
+
+sudo syncoid --compress=none zroot/usr zfs-pool-WD1TB-1/zfs-backups/usr
+
+sudo syncoid --compress=none zroot/usr/local zfs-pool-WD1TB-1/zfs-backups/usr/local
+
+sudo syncoid --compress=none zroot/data zfs-pool-WD1TB-1/zfs-backups/data
+
+sudo syncoid --compress=none zroot/data/home zfs-pool-WD1TB-1/zfs-backups/data/home
