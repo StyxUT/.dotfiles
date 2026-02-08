18e7f8b0f9a30eff32ad6bfeebb9ca63df3bb21f
styxut <styxut@gmail.com>
2026-02-08 11:36:18 -0700

    make hiden and git ignored files visible in file menu

--- nvim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/.config/nvim/lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  filters = {
+		git_ignored = false
+	},
+})
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
--- nvim/.config/nvim/lua/core/plugin_config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,11 +1,16 @@
 require('telescope').setup({
   defaults = {
+	  file_ignore_patterns = {},
     mappings = {
       i = {
         ["<C-u>"] = false,
         ["<C-d>"] = false,
       },
     },
+		pickers = {
+			find_files = {
+			  no_ignore = true,
+				hidden = true,
   },
 })
 
